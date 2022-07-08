62ab79b71fdd9fb83dce6e776cab7c3dee88780a
Satriagalangs <18102249@ittelkom-pwt.ac.id>
2022-07-08 20:00:53 +0700

    get API github

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+
+  const apiGithub = async () => {
+    const ambil = await fetch(
+      "https://api.github.com/users/Satriagalangs/repos"
+    );
+    const value = await ambil.json();
+    const hasil = value.map((isi) => {
+      return {
+        label: isi.name,
+        value: isi.name,
+      };
+    });
+  };
+
+  useEffect(() => {
+    apiGithub();
+  }, []);
 
   return (
     <div>
