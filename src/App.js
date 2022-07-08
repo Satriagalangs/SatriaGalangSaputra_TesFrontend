cce6a0eb820d2bdc0ee959e155abdd2748ca06d7
Satriagalangs <18102249@ittelkom-pwt.ac.id>
2022-07-08 19:54:34 +0700

    membuat useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
   return (
     <div>
       <h2>TES</h2>
